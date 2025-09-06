3376b189825557ab41254516d044be4fde5c969c
Gabriel <57328274+Gabriel-Cacharo@users.noreply.github.com>
2025-09-06 15:13:05 -0300

    feat: :sparkles: add video URL

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,49 +3,70 @@ import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-
-  const [ showNavBar, setShowNavBar ] = useState([ true ]);
-  const [ lastScrollY, setLastScrollY ] = useState(0);
+  const [showNavBar, setShowNavBar] = useState([true]);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
-
     const handleScroll = () => {
-
-      if( window.scrollY > lastScrollY ){
-
-        setShowNavBar( false );
-
-      }else{
-
-        setShowNavBar( true );
-
+      if (window.scrollY > lastScrollY) {
+        setShowNavBar(false);
+      } else {
+        setShowNavBar(true);
       }
 
-      setLastScrollY( window.scrollY );
-
-    }
+      setLastScrollY(window.scrollY);
+    };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-
-  }, [ lastScrollY ])
+  }, [lastScrollY]);
 
   return (
-    <nav className={`${styles.navbar} ${!showNavBar ? styles.navbar_hidden : ""}`}>
+    <nav
+      className={`${styles.navbar} ${!showNavBar ? styles.navbar_hidden : ""}`}
+    >
       <img
         src="/Logo_SODAL-Photoroom.webp"
         alt="Logo SODAL"
         className={styles.nav_logo}
       />
       <div className={styles.nav_links}>
-        <NavLink to="/" className={({ isActive }) => isActive ? styles.activeLink : '' } >INÍCIO</NavLink>
-        <NavLink to="/about" className={({ isActive }) => isActive ? styles.activeLink : '' } >SOBRE</NavLink>
-        <NavLink to="/donations" className={({ isActive }) => isActive ? styles.activeLink : '' } >DOAÇÕES</NavLink>
-        <NavLink to="/contact" className={({ isActive }) => isActive ? styles.activeLink : '' } >FALE CONOSCO</NavLink>
-        <NavLink to="/ranking" className={({ isActive }) => isActive ? styles.activeLink : '' } >RANKING</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          INÍCIO
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          SOBRE
+        </NavLink>
+        <NavLink
+          to="/donations"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          DOAÇÕES
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          FALE CONOSCO
+        </NavLink>
+        <NavLink
+          to="/ranking"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          RANKING
+        </NavLink>
+        <a href="https://www.youtube.com/watch?v=Xt_J8-pfw44" target="_blank">
+          VEJA O VÍDEO
+        </a>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
